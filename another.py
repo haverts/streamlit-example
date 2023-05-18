a4e0fdb6a2a261191f1cb3cdc91cba23cada513e
haverts <127331021+haverts@users.noreply.github.com>
2023-05-18 15:08:19 +0800

    Update another.py

--- another.py
+++ another.py
@@ -86,6 +86,44 @@ def main():
             predicted_data = scaler.inverse_transform(predicted_data)
             
         elif model == 'SARIMA':
+                        # Data scaling
+            
+            train_data = data[:-4392]
+            test_data = data[-4392:]
+            scaler = MinMaxScaler()
+            scaled_data = scaler.fit_transform(simplified_data)
+
+            # Prepare LSTM training data
+            # Data scaling
+            scaler = MinMaxScaler()
+            scaled_data = scaler.fit_transform(data)
+
+            # Prepare training data
+            train_data = scaled_data[:-8760]  # Use all but the last year for training
+            x_train, y_train = [], []
+            for i in range(8760, len(train_data)):
+                x_train.append(train_data[i-8760:i, 0])
+                y_train.append(train_data[i, 0])
+            x_train, y_train = np.array(x_train), np.array(y_train)
+            x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+            # Create and train LSTM model
+            model = Sequential()
+            model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+            model.add(LSTM(units=50))
+            model.add(Dense(units=1))
+            model.compile(optimizer='adam', loss='mean_squared_error')
+            model.fit(x_train, y_train, epochs=10, batch_size=32)
+ 
+            # Forecasting
+            last_year_data = scaled_data[-8760:]  # Use the last year for forecasting
+            x_test = []
+            for i in range(8760, len(last_year_data)):
+                x_test.append(last_year_data[i-8760:i, 0])
+            x_test = np.array(x_test)
+            x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+            predicted_data = model.predict(x_test)
+            predicted_data = scaler.inverse_transform(predicted_data)
     # Plot forecast
     trace1 = go.Scatter(x=df.index, y=df.avg_lmp, name='Actual')
     trace2 = go.Scatter(x=list(lstm_predictions.index), y=lstm_predictions.avg_lmp, name='LSTM')
