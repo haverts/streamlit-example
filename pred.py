55e915cfb22bd1298c1e28831c204bba7c6655c9
haverts <127331021+haverts@users.noreply.github.com>
2023-05-19 17:23:41 +0800

    Update pred.py

--- pred.py
+++ pred.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+import plotly.graph_objects as go
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
-import plotly.graph_objects as go
-import plotly.express as px
+from statsmodels.tsa.arima.model import ARIMA
 
 # Function to preprocess the data
 def preprocess_data(df):
@@ -56,6 +56,17 @@ def forecast_data(model, last_x, scaler):
     future_data = scaler.inverse_transform(future_data)
     return future_data
 
+# Function to build and train the ARIMA model
+def build_arima_model(data):
+    model1 = ARIMA(data, order=(1, 0, 0))
+    model1_fit = model1.fit()
+    return model1_fit
+
+# Function to forecast data using the ARIMA model
+def forecast_arima(model1, steps):
+    future_data = model1.forecast(steps=steps)[0]
+    return future_data
+
 # Streamlit app
 def main():
     st.title('LSTM Data Forecasting')
@@ -82,12 +93,39 @@ def main():
         # Display forecasted data
         st.subheader('Forecasted Data')
         st.write(forecast_df)
+        
+        model1 = build_arima_model(df['avg_lmp'])
+
+        # Forecast data for 1 day
+        steps = 24
+        last_x1 = X[-1]
+        future_data_arima = forecast_arima(model1, steps)
+        forecast_timestamps_arima = pd.date_range(start=df.index[-1], periods=len(future_data) + 1, freq='H')[1:]
+        
+                
+         # Create DataFrame for forecasted data
+        forecast_arima_df = pd.DataFrame({'Delivery Interval': forecast_timestamps_arima, 'Forecasted Value': future_data_arima[:, 0]})
+        forecast_arima_df.set_index('Delivery Interval', inplace=True)
+
+         # Display forecasted data
+        st.subheader('Forecasted Data')
+        st.write(forecast_arima_df)
 
         # Plot forecasted data
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=forecast_timestamps, y=future_data[:, 0], name='Forecasted Data'))
         fig.update_layout(title='1-Day Forecast using LSTM', xaxis_title='Delivery Interval', yaxis_title='Average LMP')
         st.plotly_chart(fig)
+        
+
+        trace2 = go.Scatter(x=list(forecast_timestamps), y=future_data, name='LSTM')
+        trace3 = go.Scatter(x=list(test_data.index), y=sarima_predictions, name='ARIMA')
+
+
+        layout = go.Layout(title='Actual vs Forecast LMP using LSTM and SARIMAX')
+        fig = go.Figure(data=[trace1, trace2, trace3], layout=layout)
+        fig.show()
+
 
 if __name__ == '__main__':
     main()
