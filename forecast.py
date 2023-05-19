51b6a4b768082ec1a598ec7735f642de1a293e38
haverts <127331021+haverts@users.noreply.github.com>
2023-05-20 01:47:29 +0800

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
 import plotly.graph_objects as go
-from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 
 # Function to preprocess the data
 def preprocess_data(df):
@@ -14,7 +14,7 @@ def preprocess_data(df):
     scaler = MinMaxScaler()
     scaled_data = scaler.fit_transform(df_hourly)
 
-    # Prepare the data for ARIMA model
+    # Prepare the data for SARIMA model
     # User input for forecasting steps
     lookback = 2160
     X = []
@@ -28,32 +28,27 @@ def preprocess_data(df):
 
     return X, y, scaler
 
-# Function to build and train the ARIMA model
+# Function to build and train the SARIMA model
 def build_model(X, y):
     # Flatten the input data
     X = X.reshape(X.shape[0], X.shape[1])
     
-    model = ARIMA(y, order=(5, 1, 0))
+    model = SARIMAX(y, order=(5, 1, 0))
     model_fit = model.fit()
 
     return model_fit
 
 # Function to forecast data
 def forecast_data(model, last_x, scaler, steps):
-    forecast = model.forecast(steps)
-    future_data = np.array(forecast[0])
-    
-    if future_data.shape[0] == 1:
-        future_data = future_data.reshape(1, 1)
-    else:
-        future_data = future_data.reshape(future_data.shape[0], 1)
-        
+    future_data = model.forecast(steps)
+    future_data = np.array(future_data)
+    future_data = future_data.reshape(future_data.shape[0], 1)
     future_data = scaler.inverse_transform(future_data)
     return future_data
 
 # Streamlit app
 def main():
-    st.title('ARIMA Data Forecasting')
+    st.title('SARIMA Data Forecasting')
 
     # File upload
     uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
@@ -81,7 +76,7 @@ def main():
         # Plot forecasted data
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=forecast_timestamps, y=future_data[:, 0], name='Forecasted Data'))
-        fig.update_layout(title='1-Day Forecast using ARIMA', xaxis_title='Delivery Interval', yaxis_title='Average LMP')
+        fig.update_layout(title='1-Day Forecast using SARIMA', xaxis_title='Delivery Interval', yaxis_title='Average LMP')
         st.plotly_chart(fig)
 
 if __name__ == '__main__':
