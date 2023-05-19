5d05dfa2d143d5aa91cbb532056c3edec34924e7
haverts <127331021+haverts@users.noreply.github.com>
2023-05-19 23:49:49 +0800

    Update pred.py

--- pred.py
+++ pred.py
@@ -141,7 +141,7 @@ def main():
         future_data_arima_y = future_data_arima[:, 0].tolist()
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=forecast_timestamps, y=future_data_lstm[:, 0], name='Forecasted Data (LSTM)', line=dict(color='blue')))
-        fig.add_trace(go.Scatter(x=forecast_timestamps, y=future_data_arima_y, name='Forecasted Data (ARIMA)', line=dict(color='red', dash='dot')))
+        fig.add_trace(go.Scatter(x=forecast_timestamps, y=forecast_df_arima, name='Forecasted Data (ARIMA)', line=dict(color='red', dash='dot')))
         fig.update_layout(
             title='1-Day Forecast using LSTM and ARIMA',
             xaxis_title='Delivery Interval',
